645fc4e912433b081bf2fd0b860920bc7a8fb461
RitzDoolabh <ritzd93@gmail.com>
2025-10-26 14:07:04 +0200

    Initial commit

---
+++ src/main/java/org/cyberknight/kore/KoreApplication.java
@@ -0,0 +1,13 @@
+package org.cyberknight.kore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KoreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KoreApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/cyberknight/kore/KoreApplicationTests.java
@@ -0,0 +1,13 @@
+package org.cyberknight.kore;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KoreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
